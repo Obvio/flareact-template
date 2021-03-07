dcaa5cb80e868905cae8007bb10d5fef65283032
Obvio <Obvio@users.noreply.github.com>
2021-03-07 13:34:12 -0300

    test

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Index() {
     return (
         <div>
             <h1>
-                X - You're running React on the Edge!
+                X2 - You're running React on the Edge!
             </h1>
 
             {
