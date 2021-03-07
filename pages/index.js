020fd4d0337e02c8051d75491f331ccc1ba3ab7f
Obvio <Obvio@users.noreply.github.com>
2021-03-07 13:26:49 -0300

    test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import React, {useEffect} from 'react'
+
 export default function Index() {
-  return (
-    <h1>
-      You're running React on the Edge!
-    </h1>
-  );
+
+    const [_res, setRes] = useState();
+    useEffect(() => {
+        fetch('https://reqres.in/api/users?page=2').then(res => res.json()).then(res => setRes(res))
+    }, [])
+
+    return (
+        <div>
+            <h1>
+                You're running React on the Edge!
+            </h1>
+
+            {
+                _res ?? _res.data.map(item => <div>{item.email}</div>)
+            }
+
+        </div>
+    );
 }
